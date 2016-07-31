92abc16549b90c053911d71de052810188b2f3b5
David Shen <me@pantherman594.com>
2016-07-30 20:51:34 -0400

    Remove unnecessary comments

--- src/main/java/io/thejunct/fly/Fly.java
+++ src/main/java/io/thejunct/fly/Fly.java
@@ -1,5 +1,5 @@
 package io.thejunct.fly;
-//Imports
+
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
@@ -8,15 +8,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.logging.Logger;
 
-//Main Source Code
 public class Fly extends JavaPlugin {
-
-
     private Logger logger;
 
-
-
-    //onEnable
     @Override
     public void onEnable() {
         PluginDescriptionFile pdfFile = this.getDescription();
@@ -24,7 +18,6 @@ public class Fly extends JavaPlugin {
         this.logger.info(pdfFile.getName() + " Has Been Activated")
     }
 
-    //onDisable
     @Override
     public void onDisable() {
         PluginDescriptionFile pdfFile = this.getDescription();
@@ -32,7 +25,6 @@ public class Fly extends JavaPlugin {
         this.logger.info(pdfFile.getName() + " Has Been Turned Off")
     }
 
-    //Commands
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         Player player = (Player) sender;
@@ -42,11 +34,8 @@ public class Fly extends JavaPlugin {
             player.setFlying();
             player.sendMessage("You Are Now Flying");
 
-
-
         }
-
     }
 
-    return false;
+    return false
 }
